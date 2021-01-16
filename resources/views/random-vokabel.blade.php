b47904d77f10bfa10577796c64728c403ec54599
leonhardwiedmann <leo@hacce.com>
2021-01-16 14:08:54 +0100

    seperated blades

--- resources/views/random-vokabel.blade.php
+++ resources/views/random-vokabel.blade.php
@@ -1,25 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
-        <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+        @include("header")
         <link rel="stylesheet" href="/css/app.css" >
         <title>Laravel</title>
         @livewireStyles
     </head>
     <body>
-      <nav class="navbar navbar-expand-lg navbar-primary">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="/">Random Vokabel</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/vokabels">Vokabels</a>
-            </li>
-          </ul>
-      </nav>
+      @include("navigation")
       <div class="container-center">
       <div class="card">
         <div class="card-body">
