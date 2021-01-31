1dd804f749286e252e675ef72ba9263d5fe59784
leonhardwiedmann <leo@hacce.com>
2021-01-31 21:22:58 +0100

    number routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('artisan/{command}', function($command){
+//     try{
+//       $artisan = Artisan::call(str_replace("_", " ", $command));
+//       $output = Artisan::output();
+//       return str_replace("\n", "</br>", $output);
+//     }
+//     catch (Exception $e) {
+//       $artisan = Artisan::call("list");
+//       $output = Artisan::output();
+//       $error = str_replace("\n", "</br>", $e);
+//       return $error . "<br><br><br>" . str_replace("\n", "</br>", $output);
+//     }
+//   });
+
 
 Route::get("/login", [\App\Http\Controllers\PublicController::class, "login"])->name("login");
 Route::get("/register", [\App\Http\Controllers\PublicController::class, "register"])->name("register");
@@ -23,4 +37,5 @@ Route::middleware("auth")->group(function(){
   Route::get('/vokabels', [\App\Http\Controllers\VokabelController::class, "addVokabel"])->name("list.vokabel");
 
   Route::get("/", [\App\Http\Controllers\VokabelController::class, "RandomVokabel"])->name("random.vokabel");
+  Route::get("/numbers", [\App\Http\Controllers\NumberController::class, "RandomNumber"])->name("random.number");
 });
